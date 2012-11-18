daeaa2ca0ce08af637331ab06d81d97b9a73039f
Paul <paul.fletcher-hill@yale.edu>
2012-11-18 12:14:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pierson::Application.config.secret_token = '55999d87ecea22ec0da76e6bb70a86d04ef82d40d4207ce673803e05aa8b8104866516e1c1294b33f8b792cbc63de2131c1abce78643222dde2650c70255ed60'
