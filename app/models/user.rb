988c88e393fafdb236219a2fb505b0368b24be9f
Paul <paul.fletcher-hill@yale.edu>
2012-11-19 11:47:26 -0500

    better subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,13 @@ class User < ActiveRecord::Base
   has_many :organizations, :through => :access_controls
   has_many :subscriptions
   has_many :subscribeable_organizations, :through => :subscriptions, :source => :subscribeable, :source_type => 'Organization'
+  has_many :organization_events, :through => :subscribeable_organizations, :source => :events
   has_many :subscribeable_categories, :through => :subscriptions, :source => :subscribeable, :source_type => 'Category'
+  has_many :category_events, :through => :subscribeable_categories, :source => :events
   
   #Subscribed events
   def events
-    events = Array.new
-    self.subscribeable_organizations.map{|s| s.events}.each do |event|
-      events << event
-    end
-    self.subscribeable_categories.map{|s| s.events}.each do |event|
-      events << event
-    end
-    events.first
+    (self.organization_events + self.category_events).uniq
   end
   
 end
