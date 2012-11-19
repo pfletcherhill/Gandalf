a56f9d1e30a8cf7d5a08e92e6e2ddb09e0c61795
Rafi Khan <faiaz.a.khan@gmail.com>
2012-11-19 00:54:35 -0600

    changed user events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,23 @@
 class User < ActiveRecord::Base
-  
-  #Associations
+
+
+  # Associations
   has_many :access_controls
   has_many :organizations, :through => :access_controls
   has_many :subscriptions
   has_many :subscribeable_organizations, :through => :subscriptions, :source => :subscribeable, :source_type => 'Organization'
   has_many :subscribeable_categories, :through => :subscriptions, :source => :subscribeable, :source_type => 'Category'
   
-  #Subscribed events
+  # Subscribed events
   def events
-    events = Array.new
-    self.subscribeable_organizations.map{|s| s.events}.each do |event|
-      events << event
+    events = []
+    self.subscribeable_organizations.each do |org|
+      events += org.events
     end
-    self.subscribeable_categories.map{|s| s.events}.each do |event|
-      events << event
+    self.subscribeable_categories.eac do |cat|
+      events += cat.events
     end
-    events.first
+    events.uniq
   end
   
 end
