abc22e2e2c9c6c1fc28d96f6706535e55221b991
Rafi Khan <faiaz.a.khan@gmail.com>
2012-11-19 03:23:26 -0600

    commit before changing associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
 class Category < ActiveRecord::Base
   
-  #Associations
+  # Associations
   has_and_belongs_to_many :events
+
   has_many :subscriptions, :as => :subscribeable
+
+  # has_many :subscribers, :through => subscriptions, :class_name => "User"
   
 end
