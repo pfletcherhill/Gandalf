3291be9f2ea70978e0d982c94255ee1fc6737379
Rafi Khan <faiaz.a.khan@gmail.com>
2012-11-26 20:28:24 -0500

    removed unused cal templates

--- db/migrate/20121118172339_create_organizations.rb
+++ db/migrate/20121118172339_create_organizations.rb
@@ -4,9 +4,10 @@ class CreateOrganizations < ActiveRecord::Migration
       t.string :name
       t.text :bio
       t.string :image
-      t.string :color #format r, g, b
+      t.string :color # format r,g,b
       
       t.timestamps
     end
   end
 end
+
