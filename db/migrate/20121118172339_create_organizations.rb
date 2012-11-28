6a8558bed49d9e795bcf53a629e8f4fac0dd65eb
Rafi Khan <faiaz.a.khan@gmail.com>
2012-11-28 02:24:38 -0500

    calendar done except multi day events

--- db/migrate/20121118172339_create_organizations.rb
+++ db/migrate/20121118172339_create_organizations.rb
@@ -4,7 +4,7 @@ class CreateOrganizations < ActiveRecord::Migration
       t.string :name
       t.text :bio
       t.string :image
-      t.string :color # format r,g,b
+      t.string :color, :default => "220,220,220" # format r,g,b
       
       t.timestamps
     end
