b612ca769a2cc57e4bd29cfb76c548bd98f46209
Rafi Khan <faiaz.a.khan@gmail.com>
2013-01-05 02:32:16 -0600

    email sending

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,11 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  # :domain               => "gmail.com",
-  :user_name            => "faiaz.a.khan", # ENV['GMAIL'],
+  :user_name            => "faiaz.a.khan@gmail.com", # ENV['GMAIL'],
   :password             => "qRuP0448", # ENV['GMAIL_PASS'],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
 
 ActionMailer::Base.default_url_options[:host] = "yalego.es"
-# ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
