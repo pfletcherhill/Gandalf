56a8c9bc3d49a4b3c847a28e4a20b20781eb8524
Rafi Khan <faiaz.a.khan@gmail.com>
2013-01-05 02:38:43 -0600

    setup with environments

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,6 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = "#{ENV['GMAIL']}@gmail.com"
+    message.to = ENV['GMAIL']
   end
 end
