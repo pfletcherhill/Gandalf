2139f81e69382982d4e4460433d0bf07a9ebaf20
Paul <paul.fletcher-hill@yale.edu>
2013-03-26 08:20:29 -0400

    improving seed, bootstrap for admin panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Seed all categories
+# Seed all categories if in production
 print("Importing categories...")
 unless Rails.env.production?
   Category.import_categories("lib/data/categories.csv")
@@ -6,22 +6,30 @@ end
 print("DONE\n")
 
 # Seed all student organizations
+# Or just admins if in production
 print("Importing student organizations...")
 unless Rails.env.production?
   Organization.import_student_organizations("lib/data/student_groups.csv")
+else
+  users = { "prf8" => "TEDxYale", "fak23" => "HackYale" }
+  users.each do |id, org|
+    u = User.create_from_directory id
+    o = Organization.find_or_create_by_name org
+    u.add_authorization_to o
+  end
 end
 print("DONE\n")
 
-# Seed this month's events
+# Scrape this month's events
 print("Scraping this month's events...")
 date = DateTime.now.strftime("%Y%m%d")
 url = "http://calendar.yale.edu/cal/opa/month/#{date}/All/?showDetails=yes"
 Event.scrape_yale_events(url)
 printf("DONE\n")
 
-# Admins
+# Promote admins
 print("Promoting admins...")
-netids = %w(fak23 prf8)
+netids = %w(prf8 fak23)
 netids.each do |id|
   u = User.where(:netid => id).first
   u.admin = true
