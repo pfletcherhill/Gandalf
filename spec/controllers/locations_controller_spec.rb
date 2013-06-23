e88d240e8f57d9373568b2df7339bf0657403ddd
Rafi Khan <faiaz.a.khan@gmail.com>
2013-06-22 18:02:02 -0700

    updated routes

---
+++ spec/controllers/locations_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe LocationsController do
+  # Not sure why this doesn't work...
+  # describe 'GET search' do
+  #   it 'returns http success' do
+  #     get :search
+  #     response.should be_success
+  #   end
+  # end
+end
