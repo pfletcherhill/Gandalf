37c234d4c5c8a52f3d6c00f45ee858e84efd020b
Rafi Khan <faiaz.a.khan@gmail.com>
2013-06-22 18:39:43 -0700

    jasmine added

---
+++ public/assets/application.js
@@ -0,0 +1,16 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.ui.datepicker
+//= require jquery.ui.autocomplete
+//= require jquery-fileupload/basic
+//= require chosen-jquery
+//= require moment
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone.modelbinder
+//= require backbone/gandalf
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require_tree .
+;
