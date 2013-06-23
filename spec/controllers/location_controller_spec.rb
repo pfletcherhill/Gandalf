f51fb05a503acca4f6dcf16e7673c60c06f7b42a
Rafi Khan <faiaz.a.khan@gmail.com>
2013-06-22 17:22:26 -0700

    tests now actually passing...

--- spec/controllers/location_controller_spec.rb
+++ spec/controllers/location_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe LocationController do
 
   describe "GET 'search'" do
+    # This also doesn't work...any idea why?
     # it "returns http success" do
     #   get 'search'
     #   response.should be_success
